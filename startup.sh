5ea2268bfd04d166a54fdb3418bcbc96949f9265
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2019-10-13 13:37:00 -0700

    reverting db migration stuff

--- startup.sh
+++ startup.sh
@@ -12,19 +12,19 @@ rm -rf ./obj
 >&2 echo "Waiting for RabbitMQ to start..."
 ./wait-for-it.sh mq:5672 -- echo "RabbitMQ Started.."
 
->&2 echo "Checking if we need to do an initial migration"
-if [ "$databaseUp" == "true" ]; then
-  result=$(dotnet ef migrations list)
-  if [ "$result" == "No migrations were found." ]; then
-    echo "Creating Migration"
-    dotnet ef migrations add InitialSetup
-    dotnet ef database update
-  else
-    echo "Migrations already exist. Not creating a new one."
-  fi
-else
-  echo "Database is not up, skipping this step"
-fi
+# >&2 echo "Checking if we need to do an initial migration"
+# if [ "$databaseUp" == "true" ]; then
+#   result=$(dotnet ef migrations list)
+#   if [ "$result" == "No migrations were found." ]; then
+#     echo "Creating Migration"
+#     dotnet ef migrations add InitialSetup
+#     dotnet ef database update
+#   else
+#     echo "Migrations already exist. Not creating a new one."
+#   fi
+# else
+#   echo "Database is not up, skipping this step"
+# fi
 
 echo "Starting Core.."
 dotnet run out/FactionCore.dll
