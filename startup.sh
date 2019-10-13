d9f256012f71418d3333aa1ffa232406ce82584f
Jared Haight <1220886+jaredhaight@users.noreply.github.com>
2019-10-13 13:10:46 -0700

    tryign to move db migrations to startup

--- startup.sh
+++ startup.sh
@@ -7,19 +7,23 @@ rm -rf ./bin
 rm -rf ./obj
 
 >&2 echo "Waiting for FactionDB to start..."
-./wait-for-it.sh db:5432 -- echo "FactionDB Started.."
+./wait-for-it.sh db:5432 -- echo "FactionDB Started.." && databaseUp="true"
 
 >&2 echo "Waiting for RabbitMQ to start..."
 ./wait-for-it.sh mq:5672 -- echo "RabbitMQ Started.."
 
 >&2 echo "Checking if we need to do an initial migration"
-result=$(dotnet ef migrations list)
-if [ "$result" == "No migrations were found." ]; then
-  echo "Creating Migration"
-  dotnet ef migrations add InitialSetup
-  dotnet ef database update
+if [ "$databaseUp" == "true" ]; then
+  result=$(dotnet ef migrations list)
+  if [ "$result" == "No migrations were found." ]; then
+    echo "Creating Migration"
+    dotnet ef migrations add InitialSetup
+    dotnet ef database update
+  else
+    echo "Migrations already exist. Not creating a new one."
+  fi
 else
-  echo "Migrations already exist. Not creating a new one."
+  echo "Database is not up, skipping this step"
 fi
 
 echo "Starting Core.."
